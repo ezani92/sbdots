0a62e24fbdeba3840fa02c95099b6bd9d31d00fc
Shaiful Ezani <shaiful@naxpansion.com>
2018-03-22 16:12:39 +0800

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Notifications\Bot;
 use App\User;
+use App\Setting;
 
 class WebhookController extends Controller
 {
@@ -14,19 +15,32 @@ class WebhookController extends Controller
 
 	    $chat_text = $request->json('message.text');
 
+	    //-------------
+
+	    $setting = new Setting;
+
+	    $setting->meta = $chat_text;
+	    $setting->meta = $chat_text;
+
+	    $setting->save();
+
+
+
+	    //-------------
+
 	    if($chat_text == '/start')
 	    {
 	    	$text = "Hello, My name is SB. Im a bot. I will help you to make your life easier. :)";
 
 	    	$admin = User::where('role',1)->first();
-			$admin->notify(new NewWithdraw($text));
+			$admin->notify(new Bot($text));
 	    }
 	    else
 	    {
 	    	$text = "Sorry, I dont understand what are you talking about. You can always type /help to view available command to talk with me";
 
 	    	$admin = User::where('role',1)->first();
-			$admin->notify(new NewWithdraw($text));
+			$admin->notify(new Bot($text));
 	    }
 
 	    return 'ok';
