74693120624c5ac6a8fafc93f26207dd921a5b84
Shaiful Ezani <shaiful@naxpansion.com>
2018-03-22 16:31:04 +0800

    test

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -17,18 +17,26 @@ class WebhookController extends Controller
 	    $chat_text = $request->json('message.text');
 
 
-	    if($chat_text == '/start')
+	    if($chat_text == '/start' || $chat_text == '/start@SbdotsBot')
 	    {
 	    	$text = "Hello, My name is SB. Im a bot. I will help you to make your life easier. :)";
 
 	    	$admin = User::where('role',1)->first();
 			$admin->notify(new Bot($text));
 	    }
-	    elseif($chat_text == '/pending')
+	    elseif($chat_text == '/pending' || $chat_text == '/pending@SbdotsBot')
 	    {
 	    	$pendingTransactionCount = Transaction::where('status',1)->count();
 
 	    	$text = "Currently we still have ".$pendingTransactionCount." pending ticket request. PLEASE SOMEONE SOLVE IT!";
+
+	    	$admin = User::where('role',1)->first();
+			$admin->notify(new Bot($text));
+	    }
+	    elseif($chat_text == '/help' || $chat_text == '/help@SbdotsBot')
+	    {
+
+	    	$text = "Here is list of command to start talking with me \n\n\start - Wake me up\n\pending - Get total pending ticket\n\help - List of command available";
 
 	    	$admin = User::where('role',1)->first();
 			$admin->notify(new Bot($text));
