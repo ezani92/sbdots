82f9aaa8be0a03b3a3cf3d1f1f863f7894691265
Shaiful Ezani <shaiful@naxpansion.com>
2018-04-10 00:19:30 +0800

    update

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -83,7 +83,7 @@
 			                			<tbody>
 			                				<tr>
 			                					<td width="60%"><strong>Total Approve Deposit</strong></td>
-			                					<td>{{ $user->transactions->where('transaction_type','deposit')->where('status',2)->count() }} (RM {{ $user->transactions->where('transaction_type','deposit')->where('status',2)->sum('amount') }})</td>
+			                					<td>{{ $user->transactions->where('transaction_type','deposit')->where('status',2)->where('deposit_type','normal')->count() }} (RM {{ $user->transactions->where('transaction_type','deposit')->where('status',2)->sum('amount') }})</td>
 			                				</tr>
 			                				<tr>
 			                					<td width="60%"><strong>Total Approve Withdraw</strong></td>
@@ -91,7 +91,7 @@
 			                				</tr>
 			                				<tr>
 			                					<td width="60%"><strong>Win Lost Rate (Withdraw - deposit)</strong></td>
-			                					<td>RM {{ $user->transactions->where('transaction_type','withdraw')->where('status',2)->sum('amount') - $user->transactions->where('transaction_type','deposit')->where('status',2)->sum('amount') }}</td>
+			                					<td>RM {{ $user->transactions->where('transaction_type','deposit')->where('status',2)->where('deposit_type','normal')->sum('amount') - $user->transactions->where('transaction_type','withdraw')->where('status',2)->sum('amount') }}</td>
 			                				</tr>
 			                			</tbody>
 			                		</table>
