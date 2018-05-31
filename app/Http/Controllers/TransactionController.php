ab378b3f58d89500a2d37f51515aff6189c75c29
Shaiful Ezani <shaiful@naxpansion.com>
2018-05-31 12:05:52 +0800

    fixed deposit transaction list

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -99,7 +99,7 @@ class TransactionController extends Controller
         $to_date = Carbon::create($arrEnd[2], $arrEnd[1], $arrEnd[0], 23, 59, 59);
 
 
-        $transactions = Transaction::where('transaction_type','deposit')->where('deposit_type','normal')->where('created_at','<=',$to_date)->where('created_at','>=',$from_date)->get();;
+        $transactions = Transaction::where('transaction_type','deposit')->where('deposit_type','normal')->where('created_at','<=',$to_date)->where('created_at','>=',$from_date)->latest();;
 
         return Datatables::of($transactions)
             ->addColumn('actions', function($transaction) {
